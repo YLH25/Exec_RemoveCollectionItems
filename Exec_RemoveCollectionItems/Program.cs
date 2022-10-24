be11d08f1baa5aae5cbb60c4953e366302121197
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 18:44:47 +0800

    加入專案檔案。

---
+++ Exec_RemoveCollectionItems/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_RemoveCollectionItems
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> list = new List<int> {1 ,2 ,3,4,5,6,7,8 };
+			list.Remove(1);
+			list.Remove(2);
+			foreach (int i in list) ;
+		}
+	}
+}
